3e625deb3b0adb3041a6a101624fd517eaba7f9f
khangvn20 <khangvnv.125010122030@vtc.edu.vn>
2025-03-22 15:12:10 +0700

    .

--- app.py
+++ app.py
@@ -97,5 +97,6 @@ def analyze_sentiment_from_form():
     else:
         return jsonify({"error": "Nhận xét không được để trống"}), 400
 
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
- app.run(debug=True, host='0.0.0.0', port=5000)
+     app.run(debug=True, host='0.0.0.0', port=port) 
